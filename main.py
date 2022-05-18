3594f324334ad8387417e88981e39f520a538cb7
unknown <alanglodek@gmail.com>
2022-05-18 14:30:04 +0200

    Refractored the code.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 PORT = 8087
 BUCKET = "BGD-project"
 DATA_PATH = r"data/all_data.json"
+OUTPUT_PATH = "output/"
 YEARS = ("2016", "2017", "2018", "2019", "2020")
 PROVINCES = {
     "POLSKA": "0",
@@ -29,7 +30,7 @@ PROVINCES = {
     "ZACHODNIOPOMORSKIE": "3200000"
 }
 KEYS_LIST = ("produkty_zbozowe", "mieso", "owoce", "warzywa", "cukier", "ryby_owoce_morza",
-            "mleko", "jogurty", "sery", "tluszcze", "jaja")
+             "mleko", "jogurty", "sery", "tluszcze", "jaja")
 
 # connection
 client = RiakClient(pb_port=PORT)
@@ -69,54 +70,43 @@ def food_sum(b, provinces, target):
                 all_years_food_type_amount[index] = float(item.replace(",", "."))
             zipped_lists = zip(all_years_food_type_amount, monthly_amount_of_food)
             monthly_amount_of_food = [round(x + y, 2) for (x, y) in zipped_lists]
-        dictionary[province] = dict(zip(YEARS,monthly_amount_of_food))
+        dictionary[province] = dict(zip(YEARS, monthly_amount_of_food))
     return dictionary
 
 
 def data_to_heatmap(b, provinces, name):
-    output={}
+    output = {}
     for province in provinces:
         all_data = get_all_years_data(b, provinces[province], name)
         for index, item in enumerate(all_data):
-            all_data[index] = round(float(item.replace(",", ".")),2)
+            all_data[index] = round(float(item.replace(",", ".")), 2)
         output[province] = dict(zip(YEARS, all_data))
-    print(output)
+    # print(output)
     return output
 
 
-def test():
-    # test 1
-    print("\nTest1: output should be 5,87")
-    print("Test1: result:", get_single_data(bucket, PROVINCES["ZACHODNIOPOMORSKIE"], "mieso", "2016"))
-
-    # test 2
-    print("\nTest2: output should be ['5,87', '5,83', '5,78', '5,57', '5,95']")
-    print("Test2: result:", get_all_years_data(bucket, PROVINCES["ZACHODNIOPOMORSKIE"], "mieso"))
+def generate_heatmaps(data, title, file):
+    plt.subplots(1, 1, figsize=(18, 8))
+    sns.heatmap(data, annot=True, linewidths=.5, cmap="YlGnBu", fmt='g')
+    plt.title(title)
+    plt.savefig(file + ".png")
 
 
 load_data(DATA_PATH, bucket)
 
-#Średnia miesięczna ilość spożywanego jedzenia dla województw w latach 2016-2020
+# Średnia miesięczna ilość spożywanego jedzenia dla województw w latach 2016-2020
 monthly_all_food_consumption_df = pd.DataFrame(food_sum(bucket, PROVINCES, KEYS_LIST)).T
 
-#Procent wydawanych pieniędzy na żywność w stosunku do dochodu dla wojewodztw w latach 2016-2020
+# Procent wydawanych pieniędzy na żywność w stosunku do dochodu dla wojewodztw w latach 2016-2020
 money_spend_for_food_df = pd.DataFrame(data_to_heatmap(bucket, PROVINCES, "wydatki_na_zywnosc")).T
 
-#Procent wydawanych pieniedzy na żywność w stosunku do wydatków dla województw w latach 2016-2020
-money_spend_in_general_df =pd.DataFrame(data_to_heatmap(bucket, PROVINCES, "wydatki_ogolem")).T
-
-#test()
+# Procent wydawanych pieniedzy na żywność w stosunku do wydatków dla województw w latach 2016-2020
+money_spend_in_general_df = pd.DataFrame(data_to_heatmap(bucket, PROVINCES, "wydatki_ogolem")).T
 
 sns.set()
 
-#ax = sns.heatmap(money_spend_in_general_df,annot=True,linewidths=.5,cmap="YlGnBu",fmt='g')
-#plt.title("Średnie miesięczne wydatki (w złotych)/os dla wojewodztw w latach 2016-2020")
-#plt.show()
+generate_heatmaps(money_spend_in_general_df, "Średnie miesięczne wydatki (w złotych)/os dla wojewodztw w latach 2016-2020", OUTPUT_PATH+"Figure_1")
+generate_heatmaps(money_spend_for_food_df, "Średnie miesięczne wydatki na żywność (w złotych)/os dla wojewodztw w latach 2016-2020", OUTPUT_PATH+"Figure_2")
+generate_heatmaps(monthly_all_food_consumption_df, "Średnia miesięczna ilość spożywanego jedzenia (w kilogramach) na osobę dla województw w latach 2016-2020", OUTPUT_PATH+"Figure_3")
 
-#ax = sns.heatmap(money_spend_for_food_df, annot=True, linewidths=.5, cmap="YlGnBu", fmt='g')
-#plt.title("Średnie miesięczne wydatki na żywność (w złotych)/os dla wojewodztw w latach 2016-2020")
-#plt.show()
-
-ax = sns.heatmap(monthly_all_food_consumption_df, annot=True, linewidths=.5, cmap="YlGnBu")
-plt.title("Średnia miesięczna ilość spożywanego jedzenia (w kilogramach) na osobę dla województw w latach 2016-2020")
 plt.show()
